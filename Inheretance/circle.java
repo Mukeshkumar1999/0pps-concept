1bbee5a3c8e795756e3e3e429434213640d3cbd0
Mukesh kumar paswan <mukeshk92578@gmail.com>
2022-12-21 09:03:23 +0530

    final coment inheritance

--- Inheretance/circle.java
+++ Inheretance/circle.java
@@ -1,4 +1,11 @@
+// Inheritance is an important pillar of OOP(Object-Oriented Programming).It is the mechanism in java by which one
+// class is
+// allowed to
+// inherit the
 
+// features(fields and methods) of another class. In Java, inheritance means creating new classes based on existing ones. A class that inherits from another class can reuse the methods and fields of that class. In addition, you can add new fields and methods to your current class as well.  
+
+// supper class or base class...or parent class...
 class Circle1 {
     int radius;
 
@@ -11,6 +18,7 @@ class Circle1 {
     }
 }
 
+// subClass or deriveClass or child class....
 class Cylender extends Circle1 {
     int height;
 
